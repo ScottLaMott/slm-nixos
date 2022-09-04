d82b9186310d8601a11f9817d17df9d79fe18e5d
Scott LaMott <slm@example.com>
2022-09-04 21:01:28 +0200

    copy from running system

--- system/configuration.nix
+++ system/configuration.nix
@@ -5,20 +5,18 @@
 { config, pkgs, ... }:
 
 { imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix ];
+  [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+  ];
 
   # Bootloader.
-  boot.loader.grub.enable = true; boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true; # Enables wireless support via 
+  networking.hostName = "nixos-one"; # Define your hostname.
+  # networking.wireless.enable = true; # Enables wireless support via
   # wpa_supplicant.
-
-  # Configure network proxy if necessary networking.proxy.default = 
-  # "http://user:password@proxy:port/"; networking.proxy.noProxy = 
-  # "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -29,23 +27,28 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.utf8";
 
-  i18n.extraLocaleSettings = { LC_ADDRESS = "de_DE.utf8"; 
-    LC_IDENTIFICATION = "de_DE.utf8"; LC_MEASUREMENT = "de_DE.utf8"; 
-    LC_MONETARY = "de_DE.utf8"; LC_NAME = "de_DE.utf8"; LC_NUMERIC = 
-    "de_DE.utf8"; LC_PAPER = "de_DE.utf8"; LC_TELEPHONE = "de_DE.utf8"; 
+  i18n.extraLocaleSettings = { LC_ADDRESS = "de_DE.utf8";
+    LC_IDENTIFICATION = "de_DE.utf8"; LC_MEASUREMENT = "de_DE.utf8";
+    LC_MONETARY = "de_DE.utf8"; LC_NAME = "de_DE.utf8"; LC_NUMERIC =
+    "de_DE.utf8"; LC_PAPER = "de_DE.utf8"; LC_TELEPHONE = "de_DE.utf8";
     LC_TIME = "de_DE.utf8";
   };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  services.xserver.windowManager.dwm.enable = true;
+  services.xserver.resolutions = [
+    {
+      x = 2048; y = 1152;
+    }
+  ];
 
   # Enable the Cinnamon Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true; 
-  services.xserver.desktopManager.cinnamon.enable = true;
-  
+  services.xserver.displayManager.lightdm.enable = true;
+
   # test -remove until dash- services.xserver.videoDrivers = [ "intel" ];
   #services.xserver.videoDrivers = [ "modesetting" ];
-  #services.xserver.useGlamor = true; 
+  #services.xserver.useGlamor = true;
 
   #services.xserver.videoDrivers = [ "intel" ];
   #services.xserver.deviceSection = ''
@@ -64,62 +67,60 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true; hardware.pulseaudio.enable = false; 
+  sound.enable = true; hardware.pulseaudio.enable = false;
   security.rtkit.enable = true; services.pipewire = {
-    enable = true; alsa.enable = true; alsa.support32Bit = true; 
+    enable = true; alsa.enable = true; alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this jack.enable = 
+    # If you want to use JACK applications, uncomment this jack.enable =
     #true;
 
-    # use the example session manager (no others are packaged yet so 
-    # this is enabled by default, no need to redefine it in your config 
+    # use the example session manager (no others are packaged yet so
+    # this is enabled by default, no need to redefine it in your config
     # for now)
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager). 
+  # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.slm = { isNormalUser = true; description = "Scott LaMott"; 
-    extraGroups = [ "networkmanager" "wheel" ]; packages = with pkgs; [
-      brave
-      btop
-    #  thunderbird # maybe later slm! 
-    ];
-  };
+  #  user account for slm / Scott LaMott
+  users.users.slm.isNormalUser = true;
+  users.users.slm.description = "Scott LaMott";
+  users.users.slm.extraGroups = [ "networkmanager" "wheel" ];
+
+  # test user
+  users.users.guest.isNormalUser = true;
+  users.users.guest.description = "G. Uest";
+  users.users.guest.extraGroups = [ "networkmanager" "wheel" ];
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run: $ nix 
+  # List packages installed in system profile. To search, run: $ nix
   # search wget
   environment.systemPackages = with pkgs; [
-   pkgs.cmatrix
-   pkgs.fd
-   pkgs.file
-   pkgs.firefox
-   pkgs.fzf
-   pkgs.git
-   pkgs.htop
-   pkgs.nmap
-   pkgs.neofetch
-   pkgs.ranger
-   pkgs.ripgrep
-   pkgs.stow
-   pkgs.tmux
-   pkgs.tree
-#   pkgs.vim
-#   pkgs.vimPlugins.fzf-vim
-#   pkgs.vimPlugins.vim-fugitive
-   pkgs.zsh
+   brave
+   dmenu
+   fd
+   file
+   firefox
+   fzf
+   git
+   htop
+   killall
+   nmap
+   neofetch
+   ranger
+   ripgrep
+   tmux
+   tree
   # move to user environment later / slm
-   pkgs.lukesmithxyz-st
+   lukesmithxyz-st
 
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are 
-  # started in user sessions. programs.mtr.enable = true; 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions. programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true; enableSSHSupport = true;
   # };
@@ -132,16 +133,16 @@
   # Enable the OpenSSH daemon. services.openssh.enable = true;
   services.openssh.enable = true;
 
-  # Open ports in the firewall. networking.firewall.allowedTCPPorts = [ 
-  # ... ]; networking.firewall.allowedUDPPorts = [ ... ]; Or disable the 
+  # Open ports in the firewall. networking.firewall.allowedTCPPorts = [
+  # ... ]; networking.firewall.allowedUDPPorts = [ ... ]; Or disable the
   # firewall altogether. networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default 
-  # settings for stateful data, like file locations and database 
-  # versions on your system were taken. It‘s perfectly fine and 
-  # recommended to leave this value at the release version of the first 
-  # install of this system. Before changing this value read the 
-  # documentation for this option (e.g. man configuration.nix or on 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database
+  # versions on your system were taken. It‘s perfectly fine and
+  # recommended to leave this value at the release version of the first
+  # install of this system. Before changing this value read the
+  # documentation for this option (e.g. man configuration.nix or on
   # https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
 
