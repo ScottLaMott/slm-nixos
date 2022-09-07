3448b312b58cbb638d495b5947ae5bcc4f003136
Scott LaMott <slm@example.com>
2022-09-07 20:36:56 +0200

    erweitert

--- cp-config.sh
+++ cp-config.sh
@@ -1,2 +1,3 @@
 # kopiert configs in git staging 
-cp ~/.config/alacritty/alacritty.yml ~/ws/slm-nixos/users/alacritty/alacritty.yml
+cp ~/.config/awesome/rc.lua            ~/ws/slm-nixos/users/awesome/rc.lua
+cp ~/.config/alacritty/alacritty.yml   ~/ws/slm-nixos/users/alacritty/alacritty.yml
