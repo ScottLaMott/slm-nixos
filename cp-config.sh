3eee1783d3378d90fcf2ce2e4d1290fbb34eb258
Scott LaMott <slm@example.com>
2022-09-18 03:13:15 +0200

    pfade angepasst

--- cp-config.sh
+++ cp-config.sh
@@ -1,8 +1,15 @@
-# kopiert configs in git staging 
+# kopiert configs in git staging bereich
 
 # user configs
+cp -r ~/.config/nixpkgs/home.nix          ~/ws/slm-nixos/users/slm
+cp -r ~/.config/nixpkgs/config.nix        ~/ws/slm-nixos/users/slm
 
-cp ~/.config/nixpkgs/home.nix          ~/ws/slm-nixos/users/slm/home.nix
+cp ~/.config/awesome/rc.lua               ~/ws/slm-nixos/users/slm/awesome/rc.lua
+cp ~/.config/alacritty/alacritty.yml      ~/ws/slm-nixos/users/slm/alacritty/alacritty.yml
 
-cp ~/.config/awesome/rc.lua            ~/ws/slm-nixos/users/slm/awesome/rc.lua
-cp ~/.config/alacritty/alacritty.yml   ~/ws/slm-nixos/users/slm/alacritty/alacritty.yml
+# nix-shells
+cp -r ~/.config/nixpkgs/mutt.nix          ~/ws/slm-nixos/shell
+cp -r ~/.config/nixpkgs/neomutt.nix       ~/ws/slm-nixos/shell
+cp -r ~/.config/nixpkgs/thunderbird.nix   ~/ws/slm-nixos/shell
+
+cp -r ~/.config/nixpkgs/spotify.nix       ~/ws/slm-nixos/shell
