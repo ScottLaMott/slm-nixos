b62b469da7a042546f6362f6b9fca54038986a64
Scott LaMott <slm@example.com>
2022-09-18 03:08:06 +0200

    home configuration verbessert

--- users/slm/home.nix
+++ users/slm/home.nix
@@ -23,18 +23,17 @@
   ### slm started here
 
   home.packages = with pkgs; [
-    alacritty
     btop
+    cmatrix
     feh
     figlet
     gdu
-    git
-    git-crypt
     gnupg
-    cmatrix
     kitty
+    lsd
     powerline-fonts
-    speedtest-cli
+    xorg.xeyes
+    # zsh-history
   ];
 
   programs.bash = {
@@ -44,7 +43,7 @@
     };
     initExtra = ''
       #
-      # declared in home.nix / by slm
+      # declared in home.nix with programs.bash.initExtra / slm
       #
       neofetch
       set -o vi
@@ -55,29 +54,48 @@
     enable = true;
     shellAliases = {
       c  = "clear";
-      # confirm before overwriting something
       cp = "cp -i";
+      ls = "lsd";
       mv = "mv -i";
       rm = "rm -i";
+      # ls = "ls --color=tty";
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+        # { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+        { name = "jeffreytse/zsh-vi-mode"; } # better vi-mode integration
+
+      ];
     };
     initExtra = ''
       #
-      # declared in home.nix /by slm
+      # declared in home.nix with programs.zsh.initExtra / slm
       #
-      neofetch
-      bindkey -v
+
+      # slm trick
+      source $HOME/ws/zsh-nix-shell/.config/zsh/zshrc
+      source $HOME/.p10k.zsh
+
+      # nice terminal colorscheme
+      /usr/bin/theme.sh dracula
+      #/usr/bin/theme.sh papercolor-dark
+      # /usr/bin/theme.sh jellybeans
     '';
   };
 
   programs.vim = {
     enable = true;
     plugins = [
+      pkgs.vimPlugins.dracula-vim
       pkgs.vimPlugins.gruvbox
       pkgs.vimPlugins.jellybeans-vim
       pkgs.vimPlugins.markdown-preview-nvim
       pkgs.vimPlugins.nord-vim
       pkgs.vimPlugins.vim-airline
       pkgs.vimPlugins.vim-airline-themes
+      pkgs.vimPlugins.vim-nix
       pkgs.vimPlugins.vim-vinegar
       pkgs.vimPlugins.vim-fugitive
     ];
@@ -93,6 +111,7 @@
       " declared in home.nix / by slm
       "
       " settings
+      set background=dark
       set scrolloff=10
       set cursorline
       set noswapfile
@@ -104,11 +123,22 @@
 
       " vim-airline settings
       " let g:airline_theme="bubblegum"
-      let g:airline_theme="violet"
+      " let g:airline_theme="violet"
+      let g:airline_theme="alduin"
 
       " set colors --> gruvbox nord jellybeans
       colorscheme jellybeans
     '';
+  };
+
+  # fzf declaration
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    defaultOptions = [
+      "--layout=reverse"
+      "--info=inline"
+      ];
   };
 
   # ??? fragezeichen
