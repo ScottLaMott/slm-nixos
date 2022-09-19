fea66f76eed95b76bc6bca3abf5a0de889f81dec
Scott LaMott <slm@example.com>
2022-09-19 14:05:41 +0200

    verbesserte fzf un zvm integration

--- users/slm/home.nix
+++ users/slm/home.nix
@@ -59,7 +59,7 @@
     };
 
   initExtra = ''
-      #
+      #--------------------------------------------------------------------------------
       # declared in home.nix with programs.zsh.initExtra / slm
       #
       echo "config.xdg.configHome = ${config.xdg.configHome} ...."
@@ -73,9 +73,14 @@
       source ${config.xdg.configHome}/powerlevel10k/.p10k.zsh
 
       # set terminal colorscheme --> papercolor-dark jellybeans dracula
-      /usr/bin/theme.sh ir-black
+      /usr/bin/theme.sh black-metal-venom
 
-      # fzf with <ctrl>-r in viins and vicmd mode
+      # init function for zvm
+      function zvm_config() {
+        ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
+      }
+      zvm_config
+      # fzf with <CTRL>-r,  in viins and vicmd mode
       zvm_after_init_commands+=('bindkey -M viins '^R' fzf-history-widget')
 
       # Use vim keys in tab complete menu:
@@ -119,7 +124,7 @@
       tabstop = 2;
     };
     extraConfig  = ''
-      "
+      "--------------------------------------------------------------------------------
       " declared in home.nix / by slm
       "
       " settings
