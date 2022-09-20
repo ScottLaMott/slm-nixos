18c13861e3cbb9bd45677496ac039cd977ae9408
Scott LaMott <slm@example.com>
2022-09-20 16:02:15 +0200

    typo ...

--- mk-link.sh
+++ mk-link.sh
@@ -3,21 +3,21 @@
 # nix system configs
 pushd /etc/nixos
 echo $(pwd)
-ln -s $HOME/ws/slm-nix/system/configuration.nix configuration.nix
+ln -s $HOME/ws/slm-nixos/system/configuration.nix configuration.nix
 popd 
 
 # nix user configs
 pushd $HOME/.config/nixpkgs
-ln -s $HOME/ws/slm-nix/users/slm/home.nix   home.nix
-ln -s $HOME/ws/slm-nix/users/slm/config.nix config.nix
+ln -s $HOME/ws/slm-nixos/users/slm/home.nix   home.nix
+ln -s $HOME/ws/slm-nixos/users/slm/config.nix config.nix
 popd 
 
 # nix user configs
 mkdir -p $HOME/.config/alacritty
 pushd $HOME/.config/alacritty
-ln -s $HOME/ws/slm-nix/users/slm/alacritty/alacritty.yml alacritty.yml
+ln -s $HOME/ws/slm-nixos/users/slm/alacritty/alacritty.yml alacritty.yml
 popd 
 mkdir -p $HOME/.config/awesome
 pushd $HOME/.config/awesome
-ln -s $HOME/ws/slm-nix/users/slm/awesome/rc.lua rc.lua
+ln -s $HOME/ws/slm-nixos/users/slm/awesome/rc.lua rc.lua
 popd 
