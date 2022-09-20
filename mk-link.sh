7f668e18850e93d6647d3de42c9ebb66cdf49968
Scott LaMott <slm@example.com>
2022-09-20 16:09:45 +0200

    bf ...

--- mk-link.sh
+++ mk-link.sh
@@ -1,22 +1,27 @@
-# create symlinks to repo
+# create symlinks to repository
 
-# nix system configs
+# -----------------------------------------------------------
+# nix system configuration
 pushd /etc/nixos
 echo $(pwd)
 ln -s $HOME/ws/slm-nixos/system/configuration.nix configuration.nix
 popd 
 
-# nix user configs
+# -----------------------------------------------------------
+# nix user configuration
 pushd $HOME/.config/nixpkgs
 ln -s $HOME/ws/slm-nixos/users/slm/home.nix   home.nix
 ln -s $HOME/ws/slm-nixos/users/slm/config.nix config.nix
 popd 
 
-# nix user configs
+# -----------------------------------------------------------
+# alacritty configuration
 mkdir -p $HOME/.config/alacritty
 pushd $HOME/.config/alacritty
 ln -s $HOME/ws/slm-nixos/users/slm/alacritty/alacritty.yml alacritty.yml
 popd 
+
+# awesome configuration
 mkdir -p $HOME/.config/awesome
 pushd $HOME/.config/awesome
 ln -s $HOME/ws/slm-nixos/users/slm/awesome/rc.lua rc.lua
