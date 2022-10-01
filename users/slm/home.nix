622e44f53df8171d0e23a4eed8d7e9f123755241
Scott LaMott <slm@example.com>
2022-10-01 10:14:51 +0200

    entfernt, nutzlos

--- users/slm/home.nix
+++ users/slm/home.nix
@@ -69,9 +69,6 @@
       # set terminal colorscheme --> papercolor-dark jellybeans dracula black-metal-nile summerfruit-dark
       /usr/bin/theme.sh monokai-soda
 
-      # fzf-history-widget with <CTRL>-r,  in viins and vicmd mode
-      # zvm_after_init_commands+=('bindkey -M viins '^R' fzf-history-widget')
-
       # remap caps to escape
       setxkbmap -option caps:escape
 
