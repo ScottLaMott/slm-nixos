c3f77ab1c2a489b574aa14eda2e0dd4c2cabf50b
Scott LaMott <slm@example.com>
2023-01-14 14:13:38 +0100

    layouts modified

--- users/slm/awesome/rc.lua
+++ users/slm/awesome/rc.lua
@@ -94,10 +94,10 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile,
     awful.layout.suit.magnifier,
     awful.layout.suit.max,
-    awful.layout.suit.floating,
+    awful.layout.suit.tile,
+    -- awful.layout.suit.floating,
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
