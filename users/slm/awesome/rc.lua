4e4c92ef3148612b2dcef55080db5c1803c3291f
Scott LaMott <slm@example.com>
2023-01-24 20:48:14 +0100

    change layout order

--- users/slm/awesome/rc.lua
+++ users/slm/awesome/rc.lua
@@ -91,9 +91,9 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
+    awful.layout.suit.tile,
     awful.layout.suit.magnifier,
     awful.layout.suit.max,
-    awful.layout.suit.tile,
     -- awful.layout.suit.floating,
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.tile.left,
